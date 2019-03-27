bf5332657c17552e84ecd714f87960f42fa90fe1
Carlos Santana <csantanapr@apache.org>
2019-03-27 13:57:11 -0400

    add ce counter id

--- processEventChange/src/server/routers/cloud-event.js
+++ processEventChange/src/server/routers/cloud-event.js
@@ -3,20 +3,12 @@ const express = require('express');
 module.exports = function (app) {
     const router = express.Router();
     app.use(express.json());
-
+    var idCounter = 1;
     router.post('/', function (req, res, next) {
         console.log(req.headers)
         console.log(req.body);
-        //res.set(req.headers)
-        /*
-        res.set('ce-specversion', req.headers['ce-specversion'])
-        res.set('ce-id', req.headers['ce-id'])
-        res.set('ce-time', req.headers['ce-time'])
-        res.set('ce-source', req.headers['ce-source'])
-        res.set('ce-type', req.headers['ce-type'])
-        */
         res.set('ce-specversion', '0.2')
-        res.set('ce-id', 'ABC-123')
+        res.set('ce-id', (idCounter++).toString())
         res.set('ce-type', 'com.example.test')
         res.send(req.body)
     });
